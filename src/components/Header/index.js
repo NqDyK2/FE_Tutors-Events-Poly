e443bc11f77cbe2786ba8ec08ddb4268d8257988
toanpqph14805 <toanpqph14805@fpt.edu.vn>
2022-09-11 23:03:24 +0700

    Revert "TEP-41 update header"
    This reverts commit 47764c1ba8bcf5033db6ef7c840de6e09edd56dd.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,29 +1,11 @@
 import { Header } from 'antd/lib/layout/layout';
 import React from 'react';
-import { Link } from 'react-router-dom';
-
-// images
-import Logo from './../../assets/images/Logo.png';
 
 const AppHeader = () => {
   return (
     <Header className='tw-bg-white' style={{ padding: 0 }}>
-      <div className='tw-flex tw-items-center tw-content-center tw-justify-between'>
-        <div>
-          <img
-            className='tw-w-[160px]'
-            src={Logo}
-            alt=''
-          />
-        </div>
-        <div>
-          <Link
-            className='tw-text-black tw-pr-[15px]'
-            to='/login'
-          >
-            ĐĂNG NHẬP
-          </Link>
-        </div>
+      <div className='tw-flex tw-items-center tw-content-center'>
+        This is the header
       </div>
     </Header>
   );
