d3249e68b76075f3c167eb5beecfe27c770b0658
duyvq1407 <duyvq1407@gmail.com>
2022-09-12 17:19:39 +0700

    TEP-43 create login page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import AuthPage from '../features/auth/pages/index.js';
 import HomePage from '../features/home-page/pages/index.js';
+import LoginPage from '../features/login-page/pages/index.js';
 import AddClassPage from '../features/tutors/pages/TutorAddClass/index.js';
 import WelcomePage from '../features/welcom-page/pages/index.js';
 import AppLayout from '../layout/AppLayout.js';
@@ -12,6 +13,7 @@ const AppRoutes = () => {
     <Routes location={location} key={location.pathname}>
       <Route path='/auth' element={<AuthPage />} />
       <Route path='/welcome' element={<WelcomePage />} />
+      <Route path='/login' element={<LoginPage />} />
       <Route path='/' element={<AppLayout />}>
         <Route index element={<HomePage />} />
         <Route path='/' element={<HomePage />} />
