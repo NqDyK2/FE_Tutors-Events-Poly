60b62d245c0d707e376ec7ca2c52aa17421e144c
duyvq1407 <duyvq1407@gmail.com>
2022-09-13 15:30:52 +0700

    TEP-48 create banner welcome page

---
+++ src/features/welcom-page/components/BannerArrow/index.js
@@ -0,0 +1,18 @@
+import {FaChevronLeft, FaChevronRight} from "react-icons/fa"
+import "./bannerarrow.css"
+
+const BannerArrow = ({ onClick, direction }) => {
+  return (
+    <button
+      className={`tw-w-9 tw-h-9 tw-absolute tw-top-1/2 tw-z-10 tw-text-[30px] tw-ease-linear tw-duration-200
+       tw-hover:text-white tw-text-[#fff] tw-hover:bg-transparent
+       ${ direction === "right" ? "tw-right-[6%]" : "tw-left-[6%]" }`
+      }
+      onClick={onClick}
+    >
+      {direction === "right" ? <FaChevronRight /> : <FaChevronLeft/>}
+    </button>
+  );
+};
+
+export default BannerArrow;
