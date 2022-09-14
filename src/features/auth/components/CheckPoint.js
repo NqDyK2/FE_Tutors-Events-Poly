f15399578ed644d18fe1d6b2f0ed5457b9585b34
Dang Quang Linh <nogamenolife.why@gmail.com>
2022-09-14 14:09:13 +0700

    fake login

--- src/features/auth/components/CheckPoint.js
+++ src/features/auth/components/CheckPoint.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useLoginQuery } from '../../../app/api/authApiSlice';
+import { setCredentials } from '../authSlice';
 
 const CheckPoint = (props) => {
   const location = useLocation();
   const checkpoint = location.search;
   console.log(checkpoint);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const {data, error, isLoading} = useLoginQuery(checkpoint)
     useEffect(() => {
-      if (data) {
-        // props.history.push('/');
-      }
+      // if (data) {
+        dispatch(setCredentials({user: null, token: null, isAuthenticated: true}));
+        navigate('/');
+      // }
     }, [data, error, checkpoint] )
     console.log(data);
     console.log(error);
