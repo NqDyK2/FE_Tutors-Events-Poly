f18b36165f5611c3fdbd1f056245117b6c575579
emtoanhungyen <phamtoannee1506@gmail.com>
2022-09-22 15:45:06 +0700

    TEP-101 update ui semmester

--- src/features/semester/pages/index.js
+++ src/features/semester/pages/index.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useGetAllSemesterQuery } from '../../../app/api/semesterApiSlice';
 
+import Img1 from './../../../assets/images/CNDT1.png';
+
 const SemesterPage = () => {
   const { data, error, isLoading } = useGetAllSemesterQuery();
   console.log(data);
   return (
     <>
-    {error && <Typography.Text type='danger'>{error.message}</Typography.Text>}
+      {/* {error && <Typography.Text type='danger'>{error.message}</Typography.Text>}
       <List
         header={<div>Semesters</div>}
         dataSource={data?.semester?.data}
@@ -19,7 +21,116 @@ const SemesterPage = () => {
             <Link to={`/sem/${item.id}`}> {item.name} </Link>
           </List.Item>
         )}
-      ></List>
+      ></List> */}
+      {/* Tạm thời comment lại nhé các cụ */}
+      <div className='tw-flex tw-flex-wrap'>
+        <div className='tw-w-[240px] tw-border bg-neutral-400 tw-rounded-[3px] tw-shadow-transparent hover:tw-opacity-[90%] tw-mx-6 tw-my-6 tw-drop-shadow-xl hover:tw-border-gray-400'>
+          <Link to='' >
+            <div>
+              <img className='tw-w-[240px] tw-rounded-t-[3px]' src={Img1} alt='' />
+            </div>
+            <div className='tw-w-max tw-h-[50px]'>
+              <Link
+                className='tw-text-black tw-text-[16px] tw-font-medium tw-leading-[50px] tw-pl-[10px] hover:tw-text-amber-400'
+                to=''
+              >
+                Học kỳ 1
+              </Link>
+            </div>
+          </Link>
+        </div>
+        <div className='tw-w-[240px] tw-border bg-neutral-400 tw-rounded-[3px] tw-shadow-transparent hover:tw-opacity-[90%] tw-mx-6 tw-my-6 tw-drop-shadow-xl hover:tw-border-gray-400'>
+          <Link to='' >
+            <div>
+              <img className='tw-w-[240px] tw-rounded-t-[3px]' src={Img1} alt='' />
+            </div>
+            <div className='tw-w-max tw-h-[50px]'>
+              <Link
+                className='tw-text-black tw-text-[16px] tw-font-medium tw-leading-[50px] tw-pl-[10px] hover:tw-text-amber-400'
+                to=''
+              >
+                Học kỳ 2
+              </Link>
+            </div>
+          </Link>
+        </div>
+        <div className='tw-w-[240px] tw-border bg-neutral-400 tw-rounded-[3px] tw-shadow-transparent hover:tw-opacity-[90%] tw-mx-6 tw-my-6 tw-drop-shadow-xl hover:tw-border-gray-400'>
+          <Link to='' >
+            <div>
+              <img className='tw-w-[240px] tw-rounded-t-[3px]' src={Img1} alt='' />
+            </div>
+            <div className='tw-w-max tw-h-[50px]'>
+              <Link
+                className='tw-text-black tw-text-[16px] tw-font-medium tw-leading-[50px] tw-pl-[10px] hover:tw-text-amber-400'
+                to=''
+              >
+                Học kỳ 3
+              </Link>
+            </div>
+          </Link>
+        </div>
+        <div className='tw-w-[240px] tw-border bg-neutral-400 tw-rounded-[3px] tw-shadow-transparent hover:tw-opacity-[90%] tw-mx-6 tw-my-6 tw-drop-shadow-xl hover:tw-border-gray-400'>
+          <Link to='' >
+            <div>
+              <img className='tw-w-[240px] tw-rounded-t-[3px]' src={Img1} alt='' />
+            </div>
+            <div className='tw-w-max tw-h-[50px]'>
+              <Link
+                className='tw-text-black tw-text-[16px] tw-font-medium tw-leading-[50px] tw-pl-[10px] hover:tw-text-amber-400'
+                to=''
+              >
+                Học kỳ 4
+              </Link>
+            </div>
+          </Link>
+        </div>
+        <div className='tw-w-[240px] tw-border bg-neutral-400 tw-rounded-[3px] tw-shadow-transparent hover:tw-opacity-[90%] tw-mx-6 tw-my-6 tw-drop-shadow-xl hover:tw-border-gray-400'>
+          <Link to='' >
+            <div>
+              <img className='tw-w-[240px] tw-rounded-t-[3px]' src={Img1} alt='' />
+            </div>
+            <div className='tw-w-max tw-h-[50px]'>
+              <Link
+                className='tw-text-black tw-text-[16px] tw-font-medium tw-leading-[50px] tw-pl-[10px] hover:tw-text-amber-400'
+                to=''
+              >
+                Học kỳ 5
+              </Link>
+            </div>
+          </Link>
+        </div>
+        <div className='tw-w-[240px] tw-border bg-neutral-400 tw-rounded-[3px] tw-shadow-transparent hover:tw-opacity-[90%] tw-mx-6 tw-my-6 tw-drop-shadow-xl hover:tw-border-gray-400'>
+          <Link to='' >
+            <div>
+              <img className='tw-w-[240px] tw-rounded-t-[3px]' src={Img1} alt='' />
+            </div>
+            <div className='tw-w-max tw-h-[50px]'>
+              <Link
+                className='tw-text-black tw-text-[16px] tw-font-medium tw-leading-[50px] tw-pl-[10px] hover:tw-text-amber-400'
+                to=''
+              >
+                Học kỳ 6
+              </Link>
+            </div>
+          </Link>
+        </div>
+        <div className='tw-w-[240px] tw-border bg-neutral-400 tw-rounded-[3px] tw-shadow-transparent hover:tw-opacity-[90%] tw-mx-6 tw-my-6 tw-drop-shadow-xl hover:tw-border-gray-400'>
+          <Link to='' >
+            <div>
+              <img className='tw-w-[240px] tw-rounded-t-[3px]' src={Img1} alt='' />
+            </div>
+            <div className='tw-w-max tw-h-[50px]'>
+              <Link
+                className='tw-text-black tw-text-[16px] tw-font-medium tw-leading-[50px] tw-pl-[10px] hover:tw-text-amber-400'
+                to=''
+              >
+                Học kỳ 7
+              </Link>
+            </div>
+          </Link>
+        </div>
+
+      </div>
     </>
   );
 };
