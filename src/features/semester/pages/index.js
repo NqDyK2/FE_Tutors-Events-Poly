9d2f97ba243e2d406a50b6b160e0de812f776bc7
Dang Quang Linh <nogamenolife.why@gmail.com>
2022-09-30 22:05:38 +0700

    TEP-132 spinner component

--- src/features/semester/pages/index.js
+++ src/features/semester/pages/index.js
@@ -1,4 +1,4 @@
-import { Button, List, Spin, Typography } from 'antd';
+import { Button, Typography } from 'antd';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useGetAllSemesterQuery } from '../../../app/api/semesterApiSlice';
@@ -6,6 +6,7 @@ import { EditOutlined, PlusCircleOutlined } from '@ant-design/icons';
 
 import Img1 from './../../../assets/images/CNDT1.png';
 import FormSemeterRef from '../components/FormSemeterRef';
+import Spinner from '../../../components/Spinner';
 
 const SemesterPage = () => {
   const { data, error, isLoading } = useGetAllSemesterQuery();
@@ -17,38 +18,44 @@ const SemesterPage = () => {
       {error && (
         <Typography.Text type='danger'>{error.message}</Typography.Text>
       )}
-      {isLoading && <Spin />}
+      <Spinner
+        loading={isLoading}
+        size='large'
+        className={
+          'tw-flex tw-h-full tw-min-h-[50vh] tw-w-full tw-items-center tw-justify-center'
+        }
+      />
       <div>
-        <div className='tw-flex tw-justify-end tw-items-center -tw-mb-4'>
+        <div className='tw-absolute tw-right-[2%] -tw-mt-4'>
           <Button
             icon={<PlusCircleOutlined />}
-            className='tw-items-center tw-flex tw-bg-white tw-text-gray-900 tw-border-slate-900 hover:tw-bg-orange-400 hover:tw-text-slate-200'
+            className='tw-flex tw-items-center tw-rounded-md tw-border-2 tw-border-none tw-bg-orange-300 tw-px-2  tw-text-slate-100 hover:tw-bg-orange-400'
             type='primary'
             onClick={() => modalRef.current.show('add')}
           >
-            Thêm học kỳ
+            Thêm kỳ học
           </Button>
         </div>
       </div>
       {data && (
-        <div className='tw-flex tw-flex-wrap'>
+        <div className='tw-flex tw-flex-wrap tw-justify-between'>
           {data?.semester?.data.map((item, index) => (
             <div
               key={index}
-              className='tw-w-[240px] tw-border bg-neutral-400 tw-rounded-[3px] tw-shadow-transparent hover:tw-opacity-[90%] tw-mx-6 tw-my-6 tw-drop-shadow-xl hover:tw-border-gray-400'
+              className='bg-neutral-400 tw-mx-6 tw-my-6 tw-w-1/5 tw-rounded-[3px] tw-border tw-shadow-transparent tw-drop-shadow-xl hover:tw-border-gray-400 hover:tw-opacity-[90%]'
             >
               <div>
                 <Link to={`/manage/sem/${item.id}`}>
                   <img
-                    className='tw-w-[240px] tw-rounded-t-[3px]'
+                    className='tw-w-full tw-rounded-t-[3px]'
                     src={Img1}
                     alt=''
                   />
                 </Link>
               </div>
-              <div className='tw-w-full tw-flex tw-items-center tw-justify-between'>
+              <div className='tw-flex tw-w-full tw-items-center tw-justify-between'>
                 <Link
-                  className='tw-text-black tw-text-[16px] tw-font-medium tw-leading-[50px] tw-pl-[10px] hover:tw-text-amber-400'
+                  className='tw-pl-[10px] tw-text-[16px] tw-font-medium tw-leading-[50px] tw-text-black hover:tw-text-amber-400'
                   to={`/manage/sem/${item.id}`}
                 >
                   {item.name}
@@ -58,15 +65,15 @@ const SemesterPage = () => {
                     shape='circle'
                     onClick={() => modalRef.current.show('edit', item)}
                     icon={<EditOutlined className='tw-text-[20px]' />}
-                    className='tw-bg-transparent tw-border-none hover:tw-bg-transparent'
+                    className='tw-border-none tw-bg-transparent hover:tw-bg-transparent'
                   />
                 </div>
               </div>
             </div>
           ))}
-          <FormSemeterRef ref={modalRef} />
         </div>
       )}
+      <FormSemeterRef ref={modalRef} />
     </>
   );
 };
