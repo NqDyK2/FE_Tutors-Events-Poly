2289d7e1582e34c88d71ecf4f2b7d4c61ac3ae9c
Dang Quang Linh <nogamenolife.why@gmail.com>
2022-09-30 16:34:07 +0700

    TEP-131 create form

--- src/features/semester/components/FormSemeterRef.js
+++ src/features/semester/components/FormSemeterRef.js
@@ -2,6 +2,7 @@ import { Form, Input, Modal } from 'antd';
 import React, { useImperativeHandle } from 'react';
 import { useEffect } from 'react';
 import { forwardRef } from 'react';
+import { toast } from 'react-toastify';
 import './styles.css';
 
 const FormSemeterRef = (props, ref) => {
@@ -27,8 +28,8 @@ const FormSemeterRef = (props, ref) => {
       setVisible(false);
     }, 2000);
 
+    toast.error('Có cái nịt :))');
   };
-
 
   return (
     <Modal
@@ -45,11 +46,10 @@ const FormSemeterRef = (props, ref) => {
     >
       <div>
         <Form form={form} layout='vertical' onFinish={onFinished}>
-          <Form.Item label='Tên học kỳ' name='name'
-          
-            rules={[
-              { required: true, message: 'Không được để trống' },
-            ]}
+          <Form.Item
+            label='Tên học kỳ'
+            name='name'
+            rules={[{ required: true, message: 'Không được để trống' }]}
           >
             <Input />
           </Form.Item>
