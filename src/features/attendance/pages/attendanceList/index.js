db7a91bd011e56b74ee07b9fcd7063328efb057e
emtoanhungyen <phamtoannee1506@gmail.com>
2022-10-12 16:06:44 +0700

    TEP-166 chau xin thua

--- src/features/attendance/pages/attendanceList/index.js
+++ src/features/attendance/pages/attendanceList/index.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { Button, Result, Table } from 'antd';
 import { useGetAttendanceListClassQuery } from '../../../../app/api/attendanceApiSlice';
 
+
 const columns = [
   {
     title: 'Ngày',
@@ -29,12 +30,13 @@ const columns = [
     title: 'Thời gian',
     dataIndex: 'cahoc',
     key: 'cahoc',
+    
   },
   {
     title: 'Điểm danh',
     key: 'action',
     render: (_, record) => (
-      <Button className='tw-w-[100px] tw-rounded-[4px] tw-bg-[#0DB27F] tw-text-white'>
+      <Button className='tw-w-[100px] tw-rounded-[4px] tw-bg-[#0DB27F] tw-text-white dark:tw-bg-[#202125] dark:tw-border-white dark:hover:tw-bg-blue-400'>
         <Link
           to={`/diem-danh/${record.class}`}
           state={{
@@ -63,14 +65,14 @@ const AttendanceList = () => {
     subCode: item.subject_code,
     cahoc: item.start_time
       ? `${moment(item.start_time).format('HH:mm')} - ${moment(
-          item.end_time
-        ).format('HH:mm')}`
+        item.end_time
+      ).format('HH:mm')}`
       : '',
   }));
   return (
     <div className='tw-w-full'>
       <div className='tw-border-b-2 tw-pb-1'>
-        <span className='tw-text-[15px]'>Điểm danh</span>
+        <span className='tw-text-[15px] dark:tw-text-white'>Điểm danh</span>
       </div>
       {/* table antd */}
       <div className='tw-mt-6'>
