d75f16e30b8352672349b8c498411756d0e887f1
emtoanhungyen <phamtoannee1506@gmail.com>
2022-10-13 09:53:36 +0700

    TEP-178-change-the-subject-list-to-the-subject-table

--- src/features/semester/pages/ListSubject.js
+++ src/features/semester/pages/ListSubject.js
@@ -1,9 +1,9 @@
-import React, { useRef } from 'react';
-import { Button, List } from 'antd';
+import React, { useRef, useState } from 'react';
+import { Button, Checkbox, Form, Input, List, Modal, Radio, Space, Spin, Table } from 'antd';
 import VirtualList from 'rc-virtual-list';
 import { Helmet } from 'react-helmet-async';
 import { Link, useParams } from 'react-router-dom';
-import { FaReply } from 'react-icons/fa';
+import { FaEdit, FaReply } from 'react-icons/fa';
 import { useGetListClassInSemesterQuery } from '../../../app/api/semesterApiSlice';
 import { EditOutlined, PlusCircleOutlined } from '@ant-design/icons';
 import FormImportExcelRef from '../components/FormImportExcelRef';
@@ -11,9 +11,84 @@ import FormImportExcelRef from '../components/FormImportExcelRef';
 const SubjectPage = () => {
   const { id } = useParams();
   const { data, error, isLoading } = useGetListClassInSemesterQuery(id);
+  const [form] = Form.useForm();
 
   const modalRef = useRef();
+  // modal antd
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  // table antd
+  const columns = [
+    {
+      title: '#',
+      dataIndex: 'key',
+      key: 'key',
+      width: '5%',
+    },
+    {
+      title: 'Lớp',
+      // dataIndex: 'name',
+      key: 'name',
+      render: (_, record) => (
+        <Link to={`/manage/class/${record.id}`}>
+          <div className='tw-capitalize'>{record.name}</div>
+        </Link>
+      )
+    },
+    {
+      title: 'Mã môn',
+      dataIndex: 'mamon',
+      key: 'mamon',
+    },
+    {
+      title: 'Giảng viên',
+      dataIndex: 'giangvien',
+      key: 'giangvien',
+    },
+    {
+      title: 'Buổi học',
+      dataIndex: 'buoihoc',
+      key: 'buoihoc',
+    },
+    {
+      title: 'Sinh viên',
+      dataIndex: 'sinhvien',
+      key: 'sinhvien',
+    },
+    {
+      title: 'Thao tác',
+      key: 'action',
+      render: (_, record) => (
+        <Space size="middle">
+          <FaEdit size={'18px'} className='tw-cursor-pointer' onClick={showModal} />
+        </Space>
+      ),
+    },
+  ];
+  const dataSource = data?.data?.map((item, index) => ({
+    key: index + 1,
+    name: item.name,
+    mamon: item.subject_code,
+    giangvien: item.default_teacher_email,
+    buoihoc: item.lessons_count,
+    sinhvien: item.class_students_count
+  }))
+  // form antd
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
 
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
   return (
     <>
       <Helmet>
@@ -41,52 +116,61 @@ const SubjectPage = () => {
       </div>
 
       <div className='tw-mt-4'>
-        {isLoading && <p>Loading...</p>}
+        {isLoading && <Spin />}
         {error && <p>Error</p>}
         {data && (
-          <List>
-            <VirtualList
-              data={data?.data}
-              key={data?.data.id}
-              height={400}
-              itemKey='id'
-              itemHeight={47}
+          <>
+            <Table
+              size="small"
+              scroll={{ y: 380 }}
+              dataSource={dataSource}
+              columns={columns}
+            />
+            <Modal
+              title="Chỉnh sửa lớp học"
+              open={isModalOpen}
+              onOk={handleOk}
+              onCancel={handleCancel}
+              width={700}
             >
-              {(item, idx) => (
-                <List.Item key={idx + 1}>
-                  <List.Item.Meta
-                    key={item.id}
-                    title={<span className='tw-uppercase'>{item.name}</span>}
-                  />
-                  <div>
-                    <Link
-                      to={`/manage/class/${item.id}`}
-                      state={{ semesterId: id, subjectId: item.id }}
-                      className='tw-mr-4'
-                    >
-                      Danh sách sinh viên
-                    </Link>
-                    <Link
-                      to={`/manage/class/lesson/${item.id}`}
-                      state={{
-                        semesterId: id,
-                        subjectId: item.id,
-                        subjectName: item.name,
-                        default_offline_class_location:
-                          item.default_offline_class_location,
-                        default_online_class_location:
-                          item.default_online_class_location,
-                        default_tutor_email: item.default_tutor_email,
-                      }}
-                      className='tw-mr-4'
-                    >
-                      Lịch học
-                    </Link>
-                  </div>
-                </List.Item>
-              )}
-            </VirtualList>
-          </List>
+              <div>
+                <Form
+                  form={form}
+                  initialValues={{
+                    lophoc: '',
+                    kyhoc: '',
+                    phonghoc: '',
+                    linkonline: '',
+                    gianvien: '',
+                    sinhvienhotro: ''
+                  }}
+                  onFinish={onFinish}
+                  onFinishFailed={onFinishFailed}
+                  layout="vertical"
+                >
+                  <Form.Item required name='lophoc' label="Lớp học">
+                    <Input />
+                  </Form.Item>
+                  <Form.Item required name='kyhoc' label="Kỳ học">
+                    <Input />
+                  </Form.Item>
+                  <Form.Item required name='phonghoc' label="Phòng học">
+                    <Input />
+                  </Form.Item>
+                  <Form.Item required name='linkonline' label="Link học online">
+                    <Input />
+                  </Form.Item>
+                  <Form.Item name='gianvien' label="Giảng viên">
+                    <Input />
+                  </Form.Item>
+                  <Form.Item name='sinhvienhotro' label="Sinh viên hỗ trợ">
+                    <Input />
+                  </Form.Item>
+
+                </Form>
+              </div>
+            </Modal>
+          </>
         )}
         <FormImportExcelRef ref={modalRef} />
       </div>
