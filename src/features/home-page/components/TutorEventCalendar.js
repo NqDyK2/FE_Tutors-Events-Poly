c9f3bc3ffc02994976220ac48649c8cd294b1459
Dang Quang Linh <nogamenolife.why@gmail.com>
2022-10-25 09:30:09 +0700

    TEP-216 fix

--- src/features/home-page/components/TutorEventCalendar.js
+++ src/features/home-page/components/TutorEventCalendar.js
@@ -5,11 +5,12 @@ import listPlugin from '@fullcalendar/list';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import tippy from 'tippy.js';
 import 'tippy.js/dist/tippy.css';
+import 'tippy.js/themes/light.css';
 
 const TutorEventCalendar = (props) => {
   return (
     <FullCalendar
-      schedulerLicenseKey='CC-Attribution-NonCommercial-NoDerivatives'
+      schedulerLicenseKey="CC-Attribution-NonCommercial-NoDerivatives"
       plugins={[dayGridPlugin, listPlugin, timeGridPlugin]}
       headerToolbar={{
         left: '',
@@ -22,6 +23,7 @@ const TutorEventCalendar = (props) => {
       firstDay={1}
       weekTextLong
       height={600}
+      viewClassNames="dark:tw-bg-dark-mode"
       handleWindowResize={true}
       windowResizeDelay={100}
       buttonText={{
@@ -32,7 +34,7 @@ const TutorEventCalendar = (props) => {
       }}
       noEventsText={'Không có sự kiện'}
       allDaySlot={false}
-      allDayText='Cả ngày'
+      allDayText="Cả ngày"
       dayHeaderFormat={{
         weekday: 'long',
       }}
@@ -49,18 +51,30 @@ const TutorEventCalendar = (props) => {
       }}
       eventMouseEnter={(info) => {
         tippy(info.el, {
-          content: info.event.extendedProps.description,
+          content: /*html*/ `
+            <div
+              class="tw-min-w-[200px] tw-max-w-[300px] tw-max-h-[300px] "
+            >
+              <div class="tw-text-lg tw-font-bold tw-mb-2 tw-text-center">Thông tin chi tiết</div>
+              <div class="tw-flex  tw-justify-between tw-items-center tw-mb-2">
+                <div class="tw-text-sm tw-font-bold">Tên sự kiện:</div>
+                <div class="tw-text-sm">${info.event.title}</div>
+
+              </div>
+            </div>
+          
+          `,
           allowHTML: true,
-          placement: 'top',
-          theme: 'dark',
-          trigger: 'mouseenter',
+          placement: 'top-start',
+          theme: 'light',
+          trigger: 'click',
           interactive: true,
           arrow: true,
           animation: 'shift-away',
           duration: [100, 100],
         });
       }}
-      initialView='dayGridMonth'
+      initialView="dayGridMonth"
       dayMaxEvents={4}
       events={props.eventsData}
     />
