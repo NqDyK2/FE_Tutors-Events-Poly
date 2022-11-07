64c897f2afd64d317769806f3d33723a1446d455
duyvq1407 <duyvq1407@gmail.com>
2022-11-08 01:43:42 +0700

    fix breadcrumb

--- src/components/AppBreadcrumb/index.js
+++ src/components/AppBreadcrumb/index.js
@@ -1,18 +1,20 @@
 import { Breadcrumb } from 'antd';
 import React from 'react';
-import { } from '@ant-design/icons';
+import {} from '@ant-design/icons';
 import { RiHome6Line } from 'react-icons/ri';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import './styles.css';
 import { useSelector } from 'react-redux';
 import { selectCurrentUser } from '../../features/auth/authSlice';
 
 const AppBreadcrumb = () => {
+  const navigate = useNavigate();
   const pathname = useLocation().pathname;
-  const { flexBreadcrumb } = useSelector(state => state.breadcrumb)
+  const { flexBreadcrumb } = useSelector((state) => state.breadcrumb);
   const [breadcrumb, setBreadcrumb] = React.useState('Trang chủ');
   const currentUser = useSelector(selectCurrentUser);
- // const breadcrumbData = [
+
+  // const breadcrumbData = [
   //   {
   //     path: '/',
   //     breadcrumbName: 'Trang chủ',
@@ -67,62 +69,38 @@ const AppBreadcrumb = () => {
   //   },
   // ];
   return (
-    // <Breadcrumb
-    //   key={pathname}
-    //   separator='|'
-    //   className='tw-flex tw-items-center tw-bg-white tw-px-4 tw-py-4 tw-shadow-sm  tw-shadow-white tw-drop-shadow-sm dark:tw-bg-[#202125] dark:tw-opacity-90 dark:tw-shadow-none'
-    // >
-    // <Breadcrumb.Item className='tw-flex tw-h-full tw-items-center'>
-    //   <Link
-    //     to='/'
-    //     className='tw-text-[#C4CFF0] hover:tw-text-blue-300 dark:tw-text-slate-100'
-    //   >
-    //     <RiHome6Line className='tw-align-text-bottom' size={18} />
-    //   </Link>
-    // </Breadcrumb.Item>
-    //   <Breadcrumb.Item className='tw-flex tw-h-full tw-items-center'>
-    //     <Link
-    //       to='/'
-    //       className='tw-text-[#C4CFF0] hover:tw-text-blue-300 dark:tw-text-slate-100'
-    //     >
-    //       <RiHome6Line className='tw-align-text-bottom' size={18} />
-    //     </Link>
-    //   </Breadcrumb.Item>
-    //   <Breadcrumb.Item className='tw-text-base tw-text-black tw-opacity-80 dark:tw-text-white dark:tw-opacity-100'>
-    //     {breadcrumb ?? 'Trang chủ'}
-    //   </Breadcrumb.Item>
-    // </Breadcrumb>
-
-
     <Breadcrumb
       key={pathname}
-      className='tw-flex tw-items-center tw-bg-white tw-px-4 tw-py-4 tw-shadow-sm  tw-shadow-white tw-drop-shadow-sm dark:tw-bg-[#202125] dark:tw-opacity-90 dark:tw-shadow-none'
+      // separator=">"
+      className="tw-flex tw-items-center tw-bg-white tw-px-4 tw-py-4 tw-shadow-sm tw-shadow-white tw-drop-shadow-sm dark:tw-bg-[#202125] dark:tw-opacity-90 dark:tw-shadow-none"
     >
-      {/* <Breadcrumb.Item>
+      {flexBreadcrumb?.length &&
+        flexBreadcrumb.map((e) => (
+          <Breadcrumb.Item key={e.path}>
+            {e.path ? (
+              <Link
+                to={e.path}
+                className="tw-text-gray-500 hover:tw-bg-transparent hover:tw-text-blue-400 dark:tw-text-slate-100"
+              >
+                {e.title === 'Home' ? <RiHome6Line size={18} /> : e.title}
+              </Link>
+            ) : (
+              <span className="tw-text-gray-700">{e.title}</span>
+            )}
+          </Breadcrumb.Item>
+        ))}
+      {/* <Breadcrumb.Item className='tw-text-base tw-text-black tw-opacity-80 dark:tw-text-white dark:tw-opacity-100'>
+        {breadcrumb ?? 'Trang chủ'}
+      </Breadcrumb.Item>
+      <Breadcrumb.Item className='tw-flex tw-h-full tw-items-center'>
         <Link
           to='/'
           className='tw-text-[#C4CFF0] hover:tw-text-blue-300 dark:tw-text-slate-100'
         >
-          <RiHome6Line size={18} />
+          <RiHome6Line className='tw-align-text-bottom' size={18} />
         </Link>
       </Breadcrumb.Item> */}
-
-      {
-        flexBreadcrumb?.length && flexBreadcrumb.map(e => (
-          <Breadcrumb.Item
-            key={e.path}
-          >
-            <Link
-              to={e.path}
-              className='tw-text-[#C4CFF0] hover:tw-text-blue-300 dark:tw-text-slate-100'
-            >
-              {e.title}
-            </Link>
-          </Breadcrumb.Item>
-        ))
-      }
     </Breadcrumb>
-
   );
 };
 
