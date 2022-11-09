b7401744be4af5174da442c7cd9006403b1ca8c3
emtoanhungyen <phamtoannee1506@gmail.com>
2022-11-09 11:06:26 +0700

    TEP-267 emtoanhy fix bug linh tinh

--- src/features/feedback/index.js
+++ src/features/feedback/index.js
@@ -1,110 +1,106 @@
-import { Table } from 'antd'
-import React from 'react'
-import { Helmet } from 'react-helmet-async'
-import { FaReply } from 'react-icons/fa'
-import { useNavigate } from 'react-router-dom'
-
-
+import { Table } from 'antd';
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import { FaReply } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
 
 const ListFeedback = () => {
-    const navigate = useNavigate();
-    let colums = [
-        {
-            title: "STT",
-            dataIndex: 'key',
-            key: 'key',
-            width: '5%'
-        },
-        {
-            title: 'Mức độ hiểu bài',
-            dataIndex: 'levelUnderstand',
-            key: 'lelevelUnderstandvel',
-            width: '10%'
-        },
-        {
-            title: 'Mức độ nhiệt tình của GV',
-            dataIndex: 'levelEnthusiasmTeacher',
-            key: 'levelEnthusiasmTeacher',
-            width: '15%',
-        },
-        {
-            title: 'Mức độ nhiệt tình của Tutor',
-            dataIndex: 'levelEnthusiasmTutor',
-            key: 'levelEnthusiasmTutor',
-            width: '15%',
-        },
-        {
-            title: 'Các vấn đề của bạn đã được giải quyết chưa?',
-            dataIndex: 'problem',
-            key: 'problem',
-        },
-        {
-            title: "Lời nhắn",
-            dataIndex: 'note',
-            key: 'note',
-        },
-    ]
+  const navigate = useNavigate();
+  let colums = [
+    {
+      title: 'STT',
+      dataIndex: 'key',
+      key: 'key',
+      width: '5%',
+    },
+    {
+      title: 'Mức độ hiểu bài',
+      dataIndex: 'levelUnderstand',
+      key: 'lelevelUnderstandvel',
+      width: '10%',
+    },
+    {
+      title: 'Mức độ nhiệt tình của GV',
+      dataIndex: 'levelEnthusiasmTeacher',
+      key: 'levelEnthusiasmTeacher',
+      width: '15%',
+    },
+    {
+      title: 'Mức độ nhiệt tình của Tutor',
+      dataIndex: 'levelEnthusiasmTutor',
+      key: 'levelEnthusiasmTutor',
+      width: '15%',
+    },
+    {
+      title: 'Các vấn đề của bạn đã được giải quyết chưa?',
+      dataIndex: 'problem',
+      key: 'problem',
+    },
+    {
+      title: 'Lời nhắn',
+      dataIndex: 'note',
+      key: 'note',
+    },
+  ];
 
-    const dataSource = [
-        {
-            key: '1',
-            id: 1,
-            levelUnderstand: 'Hiểu',
-            levelEnthusiasmTeacher: 'Nhiệt tình',
-            levelEnthusiasmTutor: 'Nhiệt tình',
-            problem: "Đã được giải quyết",
-            note: 'Nội dung phản hồi ở đây',
-        },
-        {
-            key: '2',
-            id: 2,
-            levelUnderstand: 'Hơi hơi hiểu',
-            levelEnthusiasmTeacher: 'Hơi nhơi nhiệt',
-            levelEnthusiasmTutor: 'Hơi nhơi nhiệt',
-            problem: "Chưa được giải quyết",
-            note: "Góp ý ở phần này",
-        },
+  const dataSource = [
+    {
+      key: '1',
+      id: 1,
+      levelUnderstand: 'Hiểu',
+      levelEnthusiasmTeacher: 'Nhiệt tình',
+      levelEnthusiasmTutor: 'Nhiệt tình',
+      problem: 'Đã được giải quyết',
+      note: 'Nội dung phản hồi ở đây',
+    },
+    {
+      key: '2',
+      id: 2,
+      levelUnderstand: 'Hơi hơi hiểu',
+      levelEnthusiasmTeacher: 'Hơi nhơi nhiệt',
+      levelEnthusiasmTutor: 'Hơi nhơi nhiệt',
+      problem: 'Chưa được giải quyết',
+      note: 'Góp ý ở phần này',
+    },
 
-        {
-            key: '3',
-            id: 3,
-            levelUnderstand: ' Không hiểu',
-            levelEnthusiasmTeacher: 'Không nhiệt tình',
-            levelEnthusiasmTutor: 'Không nhiệt tình',
-            problem: 'Sẽ được giải quyết trong các buổi học sắp tới',
-            note: 'UI Gvien xem phản hồi, góp ý của sinh viên tham gia học.'
-        }
-    ]
-    return (
-        <>
-            <Helmet>
-                <title>
-                    Phản hồi
-                </title>
-            </Helmet>
-            <div>
-                <div className="tw-flex tw-justify-between tw-border-b-2 tw-pb-1">
-                    <span className="tw-text-[15px] dark:tw-text-white">
-                        Hòm thư góp ý - Môn học: (Tên môn học ở đây)
-                    </span>
-                    <button
-                        onClick={() => navigate(-1)}
-                        className="tw-flex tw-items-center tw-text-blue-500 hover:tw-text-blue-700 hover:tw-bg-transparent"
-                    >
-                        <FaReply className="tw-mr-1" /> Trở lại
-                    </button>
-                </div>
-                <Table
-                    size='small'
-                    scroll={{ y: 380 }}
-                    dataSource={dataSource}
-                    columns={colums}
-                    pagination={false}
-                // loading={{ indicator: <Spinner />, spinning: `Loading...` }}
-                />
-            </div>
-        </>
-    )
-}
+    {
+      key: '3',
+      id: 3,
+      levelUnderstand: ' Không hiểu',
+      levelEnthusiasmTeacher: 'Không nhiệt tình',
+      levelEnthusiasmTutor: 'Không nhiệt tình',
+      problem: 'Sẽ được giải quyết trong các buổi học sắp tới',
+      note: 'UI Gvien xem phản hồi, góp ý của sinh viên tham gia học.',
+    },
+  ];
+  return (
+    <>
+      <Helmet>
+        <title>Phản hồi</title>
+      </Helmet>
+      <div>
+        <div className="tw-flex tw-justify-between tw-border-b-2 tw-pb-1">
+          <span className="tw-text-[15px] dark:tw-text-white">
+            Hòm thư góp ý - Môn học: (Tên môn học ở đây)
+          </span>
+          <button
+            onClick={() => navigate(-1)}
+            className="tw-flex tw-items-center tw-text-blue-500 hover:tw-bg-transparent hover:tw-text-blue-700"
+          >
+            <FaReply className="tw-mr-1" /> Trở lại
+          </button>
+        </div>
+        <Table
+          size="small"
+          scroll={{ x: 380 }}
+          dataSource={dataSource}
+          columns={colums}
+          pagination={false}
+          // loading={{ indicator: <Spinner />, spinning: `Loading...` }}
+        />
+      </div>
+    </>
+  );
+};
 
-export default ListFeedback
+export default ListFeedback;
--- src/features/teaching-schedule/index.js
+++ src/features/teaching-schedule/index.js
@@ -1,63 +1,65 @@
 import { Table, Tooltip } from 'antd';
-import React from 'react'
+import React from 'react';
 import { Helmet } from 'react-helmet-async';
-import { useGetScheduleTeachingQuery } from '../../app/api/studentApiSlice'
+import { useGetScheduleTeachingQuery } from '../../app/api/studentApiSlice';
 import { timeFormat } from '../../utils/TimeFormat';
 
 const TeachingSchedule = () => {
-
   const columns = [
     {
-      title:"STT",
-      dataIndex:'stt',
-      key:'stt',
-      render:(text) => <strong>{text}</strong>,
-      width:"5%",
+      title: 'STT',
+      dataIndex: 'stt',
+      key: 'stt',
+      render: (text) => <strong>{text}</strong>,
+      width: '5%',
     },
     {
-      title:"Thứ - Ngày/Tháng",
-      dataIndex:"day",
-      key:"day",
-      width:"12%"
+      title: 'Thứ - Ngày/Tháng',
+      dataIndex: 'day',
+      key: 'day',
+      width: '12%',
     },
     {
-      title:"Thời gian",
-      dataIndex:"time",
-      key:"time",
-      width:"10%"
+      title: 'Thời gian',
+      dataIndex: 'time',
+      key: 'time',
+      width: '10%',
     },
     {
-      title:"Môn học",
-      dataIndex:"subjects_name",
-      key:"subjects_name",
-      width:"10%",
+      title: 'Môn học',
+      dataIndex: 'subjects_name',
+      key: 'subjects_name',
+      width: '10%',
     },
     {
-      title:"Mã môn",
-      dataIndex:"subjects_code",
-      key:"subjects_code",
-      width:"8%",
+      title: 'Mã môn',
+      dataIndex: 'subjects_code',
+      key: 'subjects_code',
+      width: '8%',
     },
     {
-      title:"Hình thức",
-      dataIndex:"type",
-      key:"type",
-      width:"7%",
+      title: 'Hình thức',
+      dataIndex: 'type',
+      key: 'type',
+      width: '7%',
     },
     {
-      title:"Phòng học",
-      dataIndex:"class_location",
-      key:"class_location",
-      width:"15%",
-      render:(_,record) => record.type === "Offline" ? (<span> {record.class_location}</span>) : (
-      <Tooltip color="#FF6D28" title={`${record.class_location}`}>
-        <div className="tw-truncate">
-            <a target="blank" href={record.class_location}>
-              {record.class_location}
-            </a>
-          </div>
-      </Tooltip>
-      )
+      title: 'Phòng học',
+      dataIndex: 'class_location',
+      key: 'class_location',
+      width: '15%',
+      render: (_, record) =>
+        record.type === 'Offline' ? (
+          <span> {record.class_location}</span>
+        ) : (
+          <Tooltip color="#FF6D28" title={`${record.class_location}`}>
+            <div className="tw-truncate">
+              <a target="blank" href={record.class_location}>
+                {record.class_location}
+              </a>
+            </div>
+          </Tooltip>
+        ),
     },
     {
       title: 'Giảng viên',
@@ -100,52 +102,53 @@ const TeachingSchedule = () => {
         </Tooltip>
       ),
     },
-  ]
+  ];
   let data = [];
 
-  const {data:listSchedule} =  useGetScheduleTeachingQuery()
+  const { data: listSchedule } = useGetScheduleTeachingQuery();
 
-  if(listSchedule){
-    data = listSchedule.data.map((item,index) => {
+  if (listSchedule) {
+    data = listSchedule.data.map((item, index) => {
       return {
-        key:index,
-        stt:index+1,
+        key: index,
+        stt: index + 1,
         id: item.id,
         day: timeFormat(item.start_time.split('  ')[0]),
         type: item.type === 1 ? 'Offline' : 'Online',
-        time:`${item.start_time.slice(10, -3)} - ${item.end_time.slice(10,-3)}`,
-        class_location:item.class_location,
+        time: `${item.start_time.slice(10, -3)} - ${item.end_time.slice(
+          10,
+          -3,
+        )}`,
+        class_location: item.class_location,
         tutor_email: item.tutor_email,
         teacher_email: item.teacher_email,
         subjects_code: item.subject_code?.toUpperCase(),
         subjects_name: item.subject_name,
         content: item.content,
-      }
-    })
+      };
+    });
   }
 
   return (
     <>
-        <Helmet>
-          <title>Lịch dạy</title>
-        </Helmet>
-        <div className='tw-border-b-2 tw-pb-1'>
-          <span className="tw-text-[15px] dark:tw-text-white ">
-            Lịch dạy 
-          </span>
-        </div>
-        
-        <Table  
-          size="small"
-          key={data.key}
-          scroll={{y:380}}
-          columns={columns}
-          dataSource={data}
-          className="tw-mt-2"
-          pagination={false}
-        />
-    </>
-  )
-}
+      <Helmet>
+        <title>Lịch dạy</title>
+      </Helmet>
+      <div className="tw-border-b-2 tw-pb-1">
+        <span className="tw-text-[15px] dark:tw-text-white ">Lịch dạy</span>
+      </div>
 
-export default TeachingSchedule
+      <Table
+        size="small"
+        key={data.key}
+        scroll={{ x: 380 }}
+        columns={columns}
+        dataSource={data}
+        className="tw-mt-2"
+        pagination={false}
+      />
+    </>
+  );
+};
+
+export default TeachingSchedule;
