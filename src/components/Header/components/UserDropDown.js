a963545fcfa46cf8c5d1de405d69923d3819ca01
Dang Quang Linh <nogamenolife.why@gmail.com>
2022-11-11 11:34:10 +0700

    stylese mobile

--- src/components/Header/components/UserDropDown.js
+++ src/components/Header/components/UserDropDown.js
@@ -15,8 +15,8 @@ const LogoutButton = () => {
   };
   return (
     <Button
-      type='text'
-      className='tw-bg-blue-100 tw-text-[13px] tw-font-medium tw-text-blue-600 hover:tw-bg-blue-600 hover:tw-text-white'
+      type="text"
+      className="tw-bg-blue-100 tw-text-[13px] tw-font-medium tw-text-blue-600 hover:tw-bg-blue-600 hover:tw-text-white"
       onClick={handleLogOut}
     >
       Đăng xuất
@@ -25,35 +25,34 @@ const LogoutButton = () => {
 };
 
 const DropdownOverlay = ({ currentUser }) => {
-  console.log(currentUser);
   return (
     <>
-      <div className='tw-max-w-md tw-rounded-sm tw-bg-white tw-shadow-md md:tw-w-[150px] lg:tw-w-[400px]'>
-        <div className='tw-flex tw-h-[75px] tw-items-center tw-rounded-t-sm tw-bg-[#2E47AE]'>
-          <h4 className='tw-ml-6  tw-text-base tw-text-gray-200'>
+      <div className="tw-max-w-md tw-rounded-sm tw-bg-white tw-shadow-md md:tw-w-[150px] lg:tw-w-[400px]">
+        <div className="tw-flex tw-h-[75px] tw-items-center tw-rounded-t-sm tw-bg-[#2E47AE]">
+          <h4 className="tw-ml-6  tw-text-base tw-text-gray-200">
             {currentUser?.name} - {currentUser?.email}
           </h4>
         </div>
         <Link
-          to='/profile'
-          className=' tw-flex tw-w-full tw-items-center tw-p-4 tw-text-base tw-text-gray-600'
+          to="/profile"
+          className=" tw-flex tw-w-full tw-items-center tw-p-4 tw-text-base tw-text-gray-600"
         >
           <div>
-            <BiUserPin className='tw-mr-4 tw-h-6 tw-w-6 tw-text-emerald-600' />
+            <BiUserPin className="tw-mr-4 tw-h-6 tw-w-6 tw-text-emerald-600" />
           </div>
-          <div className='tw-flex-1 '>
-            <p className='tw-mb-0 tw-text-[13px] hover:tw-text-blue-500 '>
+          <div className="tw-flex-1 ">
+            <p className="tw-mb-0 tw-text-[13px] hover:tw-text-blue-500 ">
               Hồ sơ cá nhân
             </p>
-            <p className='tw-mb-0 tw-text-[13px] tw-text-gray-400 '>
+            <p className="tw-mb-0 tw-text-[13px] tw-text-gray-400 ">
               Thông tin cá nhân
             </p>
           </div>
           <div>
-            <AiOutlineCaretRight className='tw-ml-auto tw-h-3 tw-w-3 tw-text-blue-300' />
+            <AiOutlineCaretRight className="tw-ml-auto tw-h-3 tw-w-3 tw-text-blue-300" />
           </div>
         </Link>
-        <div className='tw-mx-4 tw-border-t tw-border-gray-100 tw-py-4'>
+        <div className="tw-mx-4 tw-border-t tw-border-gray-100 tw-py-4">
           <LogoutButton />
         </div>
       </div>
@@ -68,13 +67,13 @@ const UserDropDown = () => {
       <Dropdown
         trigger={['click']}
         overlay={<DropdownOverlay currentUser={currentUser} />}
-        placement='topLeft'
+        placement="topLeft"
       >
         <Button
-          type='text'
-          className='tw-px-2 tw-text-[12px] tw-text-neutral-600 hover:tw-bg-blue-100 dark:tw-text-slate-200 dark:hover:tw-text-white dark:hover:tw-bg-[#1b1c1f] '
+          type="text"
+          className="tw-px-2 tw-text-[12px] tw-text-neutral-600 hover:tw-bg-blue-100 dark:tw-text-slate-200 dark:hover:tw-bg-[#1b1c1f] dark:hover:tw-text-white "
         >
-          <span className='tw-text-blue-300'>Xin chào</span>,{' '}
+          <span className="tw-text-blue-300">Xin chào</span>,{' '}
           {currentUser?.name.split(' ')[0]}
         </Button>
       </Dropdown>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,15 +2,31 @@ import { Header } from 'antd/lib/layout/layout';
 import React from 'react';
 import ThemeToogle from './components/ThemeToogle';
 import UserDropDown from './components/UserDropDown';
+import { RiMenuFoldFill, RiMenuUnfoldFill } from 'react-icons/ri';
 
-const AppHeader = () => {
+const AppHeader = ({ collapsed, setCollapsed }) => {
   return (
     <Header
-      className='tw-flex tw-items-center tw-justify-end tw-gap-x-2 tw-border-b tw-bg-white tw-pr-4 dark:tw-border-b-slate-500 dark:tw-bg-[#202125]'
+      className="tw-flex tw-items-center tw-justify-between tw-gap-x-2 tw-border-b tw-bg-white tw-pr-4 dark:tw-border-b-slate-500 dark:tw-bg-[#202125]"
       style={{ padding: 0 }}
     >
-      <ThemeToogle />
-      <UserDropDown />
+      <div className="tw-ml-4 sm:tw-invisible">
+        {collapsed ? (
+          <RiMenuUnfoldFill
+            className="tw-text-[20px] tw-text-blue-400"
+            onClick={() => setCollapsed(!collapsed)}
+          />
+        ) : (
+          <RiMenuFoldFill
+            className="tw-text-[20px] tw-text-blue-400"
+            onClick={() => setCollapsed(!collapsed)}
+          />
+        )}
+      </div>
+      <div className="tw-flex tw-items-center tw-gap-x-2">
+        <ThemeToogle />
+        <UserDropDown />
+      </div>
     </Header>
   );
 };
