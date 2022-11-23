aff86ea35113744ae351a19288cf0c856eef2cb9
Dang Quang Linh <dangquanglinh48@gmail.com>
2022-11-23 19:47:38 +0700

    check is teacher

--- src/routes/guard/ManageGuard.js
+++ src/routes/guard/ManageGuard.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Navigate, Outlet, useLocation } from 'react-router-dom'
-import { selectIsAdmin, selectIsTeacher } from '../../features/auth/authSlice'
+import { selectCurrentUser, selectIsAdmin, selectIsTeacher } from '../../features/auth/authSlice'
 
 const ManageGuard = () => {
   const isAdmin = useSelector(selectIsAdmin)
-  const isTeacher = useSelector(selectIsTeacher)
+  const isTeacher = useSelector(selectCurrentUser)?.is_teacher
   const location = useLocation();
   return (
-     isAdmin || isTeacher ? <Outlet/> : <Navigate to='/' state={{from:location}} /> 
+    isAdmin || isTeacher ? <Outlet /> : <Navigate to='/' state={{ from: location }} />
   )
 }
 
