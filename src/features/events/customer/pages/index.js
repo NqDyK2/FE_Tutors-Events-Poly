0afa0d646278d23982cb5f75e521fe41f3b5727e
NqDyK2 <nguyenquyduyabc@gmail.com>
2022-12-16 20:41:50 +0700

    Done

--- src/features/events/customer/pages/index.js
+++ src/features/events/customer/pages/index.js
@@ -15,15 +15,6 @@ const EventsPage = () => {
   const { data, isLoading, error, isSuccess } = useGetAllEventQuery();
   const dispatch = useDispatch();
 
-  const compareDate = (startTime, endTime) => {
-    const currentTime = moment().format('YYYY-MM-DD HH:mm');
-    if (currentTime.localeCompare(startTime) == -1) {
-      return "Tương lai"
-    } else if (currentTime.localeCompare(endTime) == 1) {
-      return "Đã qua"
-    }
-    return "Đang diễn ra"
-  }
 
   useEffect(() => {
     dispatch(
@@ -50,11 +41,7 @@ const EventsPage = () => {
                   <div className='tw-flex-1 '>
                     <span className='tw-text-xl dark:tw-text-white'> {item.name} </span> <br />
                   </div>
-                  <span className=''>
-                    {
-                      compareDate(item.start_time, item.end_time)
-                    }
-                  </span>
+
                   <DetailEventModal content={item} /> <br />
                   {/* <Button className=' tw-mt-5 tw-hover:bg-gradient-to-bl tw-focus:ring-4 tw-focus:outline-none tw-focus:ring-cyan-300 tw-dark:focus:ring-cyan-800 tw-mr-2 tw-mb-2 tw-w-32 tw-rounded-lg tw-bg-gradient-to-r tw-from-cyan-500 tw-border-transparent tw-to-blue-500 tw-text-center  tw-text-sm tw-font-medium tw-text-white'>
                     Xem thêm
