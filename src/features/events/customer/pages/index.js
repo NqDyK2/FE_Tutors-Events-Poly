55330b2393d4a87cf658639fb7e0e990dec2b09b
Dang Quang Linh <dangquanglinh48@gmail.com>
2022-12-16 13:34:18 +0700

    linh dq responsive

--- src/features/events/customer/pages/index.js
+++ src/features/events/customer/pages/index.js
@@ -24,13 +24,13 @@ const EventsPage = () => {
     <>
       {data && (
         <div>
-          <Row gutter={[24, 16]}>
+          <div className='tw-grid-cols-1 md:tw-gap-6 tw-grid md:tw-grid-cols-2 lg:tw-grid-cols-4' >
             {
               data.data?.map((item, index) => (
-                <Col key={index} span={6} className='tw-text-center tw-w-96 truncate tw-flex tw-flex-col'>
+                <div className='tw-text-center tw-w-full truncate tw-flex tw-flex-col'>
                   <div className='tw-w-full  tw-max-h-[150px] tw-mb-2 '>
 
-                    <img src={`${process.env.REACT_APP_API_URL}/${item.image}`} className=' tw-h-full tw-object-cover' />
+                    <img src={`${process.env.REACT_APP_API_URL}/${item.image}`} className=' tw-h-full tw-w-full tw-object-cover' />
                   </div>
                   <div className='tw-flex-1 '>
                     <span className='tw-text-xl dark:tw-text-white'> {item.name} </span> <br />
@@ -44,10 +44,10 @@ const EventsPage = () => {
                   {/* <Button className=' tw-mt-5 tw-hover:bg-gradient-to-bl tw-focus:ring-4 tw-focus:outline-none tw-focus:ring-cyan-300 tw-dark:focus:ring-cyan-800 tw-mr-2 tw-mb-2 tw-w-32 tw-rounded-lg tw-bg-gradient-to-r tw-from-cyan-500 tw-border-transparent tw-to-blue-500 tw-text-center  tw-text-sm tw-font-medium tw-text-white'>
                     Xem thêm
                   </Button> */}
-                </Col>
+                </div>
               ))
             }
-          </Row>
+          </div>
         </div>
       )}
     </>
